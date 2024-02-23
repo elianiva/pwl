465385c3ffa07344747ba6783613bad45f261b89
elianiva <dicha.arkana03@gmail.com>
2024-02-23 14:45:19 +0700

    feat: add bogus routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserProfileController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,9 +22,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return 'Hello World';
-})->name('hello');
+Route::get('/hello', [WelcomeController::class, 'hello'])->name('hello');
 
 Route::get('/world', function () {
     return 'World';
@@ -28,3 +31,31 @@ Route::get('/world', function () {
 Route::get('/user/{name?}', function (string $name = 'John') {
     return "My name is " . $name;
 });
+
+Route::get('/posts/{post}/comments/{comment}', function (string $post, string $comment) {
+    return "Post: " . $post . " Comment: " . $comment;
+});
+
+Route::middleware(['first', 'second'])->group(function () {
+    Route::get('/', function () {
+        // Uses first & second Middleware
+    });
+    Route::get('/user/profile', [UserProfileController::class, 'show']);
+});
+
+Route::domain('{account}.example.com')->group(function () {
+    Route::get('user/{id}', function ($account, $id) {
+        //
+    });
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/post', [PostController::class, 'index']);
+    Route::get('/event', [EventController::class, 'index']);
+});
+
+Route::redirect('/here', '/there');
+
+Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
+
